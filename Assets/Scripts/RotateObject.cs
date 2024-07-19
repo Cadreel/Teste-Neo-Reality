dedec957900986bc2c481ba7f36223b9bc16293c
Cadreel(Cleber) <cleber@antrathus.com.br>
2024-07-19 16:13:06 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public float rotaionSpeed = 10f;
+
+    private bool isDraggin = false;
+    private Vector3 lastMousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
